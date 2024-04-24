cde058677b6d72b2731319e539fd8e1fdecbfb2b
alivia-23 <guin.al@husky.neu.edu>
2024-04-24 00:25:47 -0700

    call download function from js script

--- js/script.js
+++ js/script.js
@@ -120,3 +120,33 @@ window.onload = function() {
     I love solving problems. I have worked on multiple technologies like Java, Javascript, Python, SQL, HTML, React, Ruby on Rails, Angular,  CSS, MySQL, PosgreSQL.\
     I am passionate about implementing and launching new projects."
 };
+
+function DownloadFile(fileName) {
+    //Set the File URL.
+    var url = "images/" + fileName;
+
+    //Create XMLHTTP Request.
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    req.onload = function () {
+        //Convert the Byte Data to BLOB object.
+        var blob = new Blob([req.response], { type: "application/octetstream" });
+
+        //Check the Browser type and download the File.
+        var isIE = false || !!document.documentMode;
+        if (isIE) {
+            window.navigator.msSaveBlob(blob, fileName);
+        } else {
+            var url = window.URL || window.webkitURL;
+            link = url.createObjectURL(blob);
+            var a = document.createElement("a");
+            a.setAttribute("download", fileName);
+            a.setAttribute("href", link);
+            document.body.appendChild(a);
+            a.click();
+            document.body.removeChild(a);
+        }
+    };
+    req.send();
+};
